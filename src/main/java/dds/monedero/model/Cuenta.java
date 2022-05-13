039d6edb8a68c3f6c9ea286db791d23f1aa73784
Lucas Barrientos <lbarrientos@frba.utn.edu.ar>
2022-05-13 00:18:44 -0300

    mejoro declaratividad

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -40,7 +40,7 @@ public class Cuenta {
 
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .filter(movimiento -> movimiento.isExtraccion() && movimiento.esDeLaFecha(fecha))
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
